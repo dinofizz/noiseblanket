f47a09d06f5bd4a9c7bd507a2fe9adb652393dc3
dinofizz <fizzotti@gmail.com>
2017-08-09 23:26:46 +0200

    [Dino] Adding IRRecorder

---
+++ IRRecorder/IRRecorder.ino
@@ -0,0 +1,42 @@
+/*
+
+   IR Recorder: Modified version of IRrecvDemo by Ken Shirrif, see
+   original copyright notice below.
+
+   I have simply modified the output to print the received codes in
+   unsigned long format rather than hexadecimal. This makes it easier
+   to use the values recorded for comparison later.
+  
+   ----
+ 
+   IRremote: IRrecvDemo - demonstrates receiving IR codes with IRrecv
+   An IR detector/demodulator must be connected to the input RECV_PIN.
+   Version 0.1 July, 2009
+   Copyright 2009 Ken Shirriff
+   http://arcfn.com
+*/
+
+#include <IRremote.h>
+
+int RECV_PIN = 11;
+
+IRrecv irrecv(RECV_PIN);
+
+decode_results results;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("IR Recorder");  
+  irrecv.enableIRIn(); // Start the receiver
+}
+
+void loop() {
+  if (irrecv.decode(&results)) {
+    unsigned long ir_result = results.value;
+    Serial.println(ir_result);    
+    irrecv.resume(); // Receive the next value
+  }
+  
+  delay(100);
+}
